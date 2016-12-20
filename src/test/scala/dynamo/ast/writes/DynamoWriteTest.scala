c38939e4fba851b84e57ba4909faacef9caaa183
lforite <forite.louis@gmail.com>
2016-12-20 15:31:36 +0100

    adding more tests

--- src/test/scala/dynamo/ast/writes/DynamoWriteTest.scala
+++ src/test/scala/dynamo/ast/writes/DynamoWriteTest.scala
@@ -1,5 +1,6 @@
 package dynamo.ast.writes
 
+import cats.implicits._
 import dynamo.ast.{M, S}
 import org.specs2.Specification
 
@@ -8,6 +9,8 @@ class DynamoWriteTest extends Specification { def is = s2"""
     writing to a valid path should yield an M with the corresponding type $write
     writing to a valid path an Option should yield an M with the corresponding type $writeOptPresent
     writing to a valid path an empty Option should yield an empty M $writeOptAbsent
+    the product of two writers should yield a M with two fields $writeProduct
+    the product of two writers writing the same location should yield a M with one field erased by the latest value $writeProductErase
 """
 
   def write = {
@@ -20,5 +23,21 @@ class DynamoWriteTest extends Specification { def is = s2"""
 
   def writeOptAbsent = {
     DynamoWrite.writeOpt[String].at("test").write(None) should_== M(List())
+  }
+
+  def writeProduct = {
+    val writer: DynamoWrite[(String, String)] =
+    (DynamoWrite.write[String].at("field1")
+        |@| DynamoWrite.write[String].at("field2")).contramap[(String, String)](z => (z._1, z._2))
+
+    writer.write(("value1", "value2")) should_== M(List("field1" -> S("value1"), "field2" -> S("value2")))
+  }
+
+  def writeProductErase = {
+    val writer: DynamoWrite[(String, String)] =
+      (DynamoWrite.write[String].at("field1")
+          |@| DynamoWrite.write[String].at("field1")).contramap[(String, String)](z => (z._1, z._2))
+
+    writer.write(("value1", "value2")) should_== M(List("field1" -> S("value2")))
   }
 }
